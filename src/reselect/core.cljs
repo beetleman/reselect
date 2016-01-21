cabf9f5678529a64daa6318232247629d4fe4617
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-01-21 19:06:08 +0100

    skeleton

--- src/reselect/core.cljs
+++ src/reselect/core.cljs
@@ -1,20 +1,55 @@
 (ns reselect.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [dommy.utils :refer [->Array]]
+            [dommy.core :as dom :refer-macros [sel sel1]]))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(defn logger [& args]
+  (when-not (empty? args)
+    (-> (first args) clj->js js/console.debug)
+    (recur (rest args))))
 
-;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
+(add-watch app-state :logger #(logger %4))
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(defn hidden-options [options multiple]
+  [:div
+   [:h1 (:text @app-state)]
+   (into [:select {:multiple multiple
+                   :name "todo!"
+                   :style {:display "none"}}]
+         (map (fn [x] [:option {:value (:value x)}
+                       (:text x)]) options))])
 
-(reagent/render-component [hello-world]
-                          (. js/document (getElementById "app")))
+(defn options->map [options]
+  (map
+   (fn [o]
+     {:value (dom/value o) :text (dom/text o)})
+   options))
 
+(defn sel-selected-options [target]
+  (->Array (.-selectedOptions (dom/sel1 target))))
+
+(defn select-multiple? [target]
+  (-> (dom/sel1 target) (dom/attr :multiple) string?))
+
+(defn get-select-name [target]
+  (-> (dom/sel1 target) (dom/attr :name)))
+
+(defn sel-options [target]
+  (dom/sel [target "option"]))
+
+(defn ^:export create [target_id]
+  (let [selector [target_id :select]
+        options (options->map (sel-options selector))
+        multiple (select-multiple? selector)
+        name (get-select-name selector)
+        selected-options (options->map (sel-selected-options selector))]
+    (logger multiple)
+    (reagent/render-component [hidden-options options multiple]
+                              (sel1 target_id))))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
